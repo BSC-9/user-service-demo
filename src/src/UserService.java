8f7c4800401de995f102e79bfb06c156afc42e20
BSCAIML <bc0985@srmist.edu.in>
2025-05-01 15:06:57 +0530

    Main branch changes

--- src/src/UserService.java
+++ src/src/UserService.java
@@ -1,6 +1,6 @@
 public class UserService {
     public String getUser() {
-        return "User from main branch";
+        return "User from main updated branch";
     }
 
     public static void main(String[] args) {
